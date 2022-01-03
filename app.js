f9b42c43b12a0f4831cb0a72190e02c1290aa657
BeyazGolge <husamcahit@gmail.com>
2022-01-03 14:57:18 +0300

    Added filtering by category

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute.js");
 const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
 const app = express();
 
 //Connect DB
@@ -30,6 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 //Routes
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
+app.use("/categories", categoryRoute);
 
 const port = 3000;
 app.listen(port, () => {
