a0e014dbf959b9ee2fac6199345deddd4d49b504
BeyazGolge <husamcahit@gmail.com>
2022-01-03 17:14:36 +0300

    Added register functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute.js");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 const app = express();
 
 //Connect DB
@@ -32,6 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = 3000;
 app.listen(port, () => {
