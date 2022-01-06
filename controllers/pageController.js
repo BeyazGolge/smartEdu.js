0bef7bb70d902f53589e10372a82454b65dac4f8
BeyazGolge <husamcahit@gmail.com>
2022-01-06 21:42:55 +0300

    Contact page added but does not work at the moment

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render("index", {
@@ -18,4 +20,42 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render("login", {
     page_name: "login",
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render("contact", {
+    page_name: "contact",
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+  // const outputMessage = `
+  // <h1>Mail Details</h1>
+  // <ul>
+  // <li> Name: ${req.body.name}</li>
+  // <li> Email: ${req.body.email}</li>
+  // </ul>
+  // <h1>Message</h1>
+  // <p>${req.body.message}</p>
+  // `;
+
+  // let transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: "",
+  //     pass: "",
+  //   },
+  // });
+
+  // let info = await transporter.sendMail({
+  //   from: "'Smart EDU Contact Form' <husamcahit@gmail.com' ",
+  //   to: "",
+  //   subject: "",
+  //   text: "",
+  //   html: outputMessage,
+  // });
+  console.log(req.body);
+  res.redirect("/");
 };
